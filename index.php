d29861429f69b21103f8ae4376d28fe1efb5b04f
setanya <s3759922110@gmail.com>
2020-09-14 19:24:19 +0300

    add title

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
+<?php
+$title = "My Page";
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test </title>
+    <title><?=$title;?> </title>
 </head>
 <body>
     <h1>Привет </h1>
